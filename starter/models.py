af972b552f0f50c358205919a41823dd287fbe6b
mparacarine <mpara.carine@gmail.com>
2020-03-02 00:04:36 +0100

    Feature: update the models for NearEarthObject

--- starter/models.py
+++ starter/models.py
@@ -2,14 +2,21 @@ class NearEarthObject(object):
     """
     Object containing data describing a Near Earth Object and it's orbits.
 
-    # TODO: You may be adding instance methods to NearEarthObject to help you implement search and output data.
+    # You may be adding instance methods to NearEarthObject to help you implement search and output data.
     """
 
     def __init__(self, **kwargs):
         """
         :param kwargs:    dict of attributes about a given Near Earth Object, only a subset of attributes used
         """
-        # TODO: What instance variables will be useful for storing on the Near Earth Object?
+        # What instance variables will be useful for storing on the Near Earth Object?
+        self.id = kwargs.get('id', None)
+        self.name = kwargs.get('name', 'No name')
+        self.diameter_min_km = float(kwargs.get(
+            'estimated_diameter_min_kilometers', 0))
+        self.is_potentially_hazardous_asteroid = kwargs.get(
+            'is_potentially_hazardous_asteroid', False)
+        self.orbits = []
 
     def update_orbits(self, orbit):
         """
@@ -19,14 +26,28 @@ class NearEarthObject(object):
         :return: None
         """
 
-        # TODO: How do we connect orbits back to the Near Earth Object?
+        # How do we connect orbits back to the Near Earth Object?
+        self.orbits.append(orbit)
+
+    def __str__(self):
+        """Print out information about the object"""
+
+        return 'name:{} orbit_dates:{} harzardous_asteriod:{}'.format(self.name,[orbit.close_approach_date for orbit in self.orbits],
+                                                                                            self.is_potentially_hazardous_asteroid)
+
+    def __repr__(self):
+        """Print out information about the object"""
+
+        return 'name:{} orbit_dates:{} harzardous_asteriod:{}'.format(self.name,
+                                                                    [orbit.close_approach_date for orbit in self.orbits],
+                                                                    self.is_potentially_hazardous_asteroid)
 
 
 class OrbitPath(object):
     """
     Object containing data describing a Near Earth Object orbit.
 
-    # TODO: You may be adding instance methods to OrbitPath to help you implement search and output data.
+    # You may be adding instance methods to OrbitPath to help you implement search and output data.
     """
 
     def __init__(self, **kwargs):
@@ -34,3 +55,14 @@ class OrbitPath(object):
         :param kwargs:    dict of attributes about a given orbit, only a subset of attributes used
         """
         # TODO: What instance variables will be useful for storing on the Near Earth Object?
+        self.name_of_neo = kwargs.get('name', 'No name')
+        self.close_approach_date = kwargs.get('close_approach_date', None)
+        self.miss_distance_kilometers = float(
+            kwargs.get('miss_distance_kilometers', 0))
+
+    def __str__(self):
+        """returns information about the object"""
+
+        return 'OrbitPath name_of_neo:{} close_approach_date:{} missed_distance_kilometers:{}'.format(self.name_of_neo,
+                                                                                                   self.close_approach_date,
+                                                                                                   self.miss_distance_kilometers)
